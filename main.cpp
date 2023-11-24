d46f8d6b2699ebcd6bf779a5d172ccb53d16e2b1
oriolcatasus-est-upc <oriol.catasus.i@estudiantat.upc.edu>
2023-11-24 18:59:41 +0000

    Primera parte

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include "mbed.h"
 
+Serial pc(USBTX, USBRX);
+CAN can(p30, p29);
+
+CANMessage msg;
+
 // main() runs in its own thread in the OS
 int main()
 {
+    pc.printf("Starting program...\r\n");
+    can.mode(CAN::Normal);
+    can.frequency(500*1000); // 500Kbps
     while (true) {
-
+        if (can.read(msg)) {
+            pc.printf("Message received: ");
+            for (int i = 0; i < sizeof(msg.data); ++i) {
+                pc.printf("%d", msg.data[i]);
+            }
+            pc.printf("\r\n");
+            char test[] = "Hola";
+            CANMessage resp;
+            resp.id = 1337;
+            resp.len = sizeof(test);
+            memcpy(resp.data, test, resp.len);
+            if (can.write(resp)) {
+                pc.printf("Message sent!!\r\n");
+            }
+        }
+        thread_sleep_for(1);
     }
 }
-
